b78864e23444163b2b6a6d91f10ae6a2b542a2c9
xinrunfeng <854645907@qq.com>
2019-03-30 18:42:24 +0800

    第一次提交

---
+++ src/main/java/com/example/memcachedemo/MemcachedemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.memcachedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemcachedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemcachedemoApplication.class, args);
+    }
+
+}
+
